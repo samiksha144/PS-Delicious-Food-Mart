42a7169b35a699a88f454fb07ea5d9edab8370f9
samiksha144 <samiksha144@gmail.com>
2022-06-22 11:39:09 +0530

    done

--- ABCD/Program.cs
+++ ABCD/Program.cs
@@ -1,12 +1,1 @@
-﻿using System;
-
-namespace ABCD
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
-}
+﻿
